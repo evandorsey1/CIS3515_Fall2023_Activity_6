c45e4c811230e4abbe60246dc08dc17e28aae15e
Evan Dorsey <dorseyevan@gmail.com>
2023-10-03 15:03:13 -0400

    implemented lambda body to launch new activity and pass value. Need to go back and finish step 1.

--- app/src/main/java/edu/temple/activities/MainActivity.kt
+++ app/src/main/java/edu/temple/activities/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,8 @@ import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
+
+const val MESSAGE_KEY = "Message"
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +28,10 @@ class MainActivity : AppCompatActivity() {
             // TODO Step2: Implement lambda body to launch new activity and pass value
             adapter = TextSizeAdapter(textSizes){
 
+                val launchIntent = Intent(this@MainActivity, DisplayActivity::class.java)
+                launchIntent.putExtra(MESSAGE_KEY, textSizes)
+                startActivity(launchIntent)
+
             }
             layoutManager = LinearLayoutManager(this@MainActivity)
         }
@@ -40,10 +47,20 @@ class TextSizeAdapter (private val textSizes: Array<Int>, callback: (Int)->Unit)
 
     // TODO Step 1: Complete onClickListener to return selected number
     inner class TextSizeViewHolder(val textView: TextView) : RecyclerView.ViewHolder (textView) {
+
+
         init {
-            textView.setOnClickListener {  }
+            textView.setOnClickListener {
+                val selectedItem = textSizes[adapterPosition]
+
+
+            }
         }
     }
+
+
+
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextSizeViewHolder {
         return TextSizeViewHolder(TextView(parent.context).apply { setPadding(5, 20, 0, 20) })
